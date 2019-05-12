c86d75c767b8546177258ae9d828c9ac6e4e41e7
Shwetha <sa_shwetha@hotmail.com>
2019-05-12 18:10:47 +0530

    Adding function for login request

---
+++ src/common/utils/loginRequest.js
@@ -0,0 +1,14 @@
+export const loginRequest = async (login, password) => {
+
+    return await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve()
+      }, 200)
+    }).then(() => {
+      if (login === 'hruday@gmail.com' && password === 'hruday123') {
+        return 'success'; // Mocked Success Token
+      } else {
+        return 'invalid'; // Mocked Failure Token
+      }
+    })
+  } 
